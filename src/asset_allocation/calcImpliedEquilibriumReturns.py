b759f0efd377c00df502df9e54d251cb8f721bdf
mdigreg2 <mdigreg2@stevens.edu>
2020-03-05 18:18:40 -0500

    prettier

--- src/asset_allocation/calcImpliedEquilibriumReturns.py
+++ src/asset_allocation/calcImpliedEquilibriumReturns.py
@@ -23,62 +23,37 @@ def getDailyReturns(equity_df, basis="Adj Close"):
 
 def getCovarianceMatrixOfReturns(equities, start, end, basis="Adj Close"):
 #This implementation is based off of this example https://stattrek.com/matrix-algebra/covariance-matrix.aspx
+    #when printed pandas will round off the output values, in reality they are float64
+    #the same thing will happen when you print the .values of V
 
     equity_df = getTickerStats(equities, start=start, end=end)
-
     basis_columns = getDailyReturns(equity_df)
 
 ########################################TESTING CONTENT############################################
 #each sub list represents a different "student's test scores" in math english and art
-
-    # basis_columns = [[90, 60, 90],
-    #                  [90, 90, 30],
-    #                  [60, 60, 60],
-    #                  [60, 60, 90],
-    #                  [30,30,30]]
-
     # basis_columns = [[90,90,60,60,30], [60,90,60,60,30], [90,30,60,90,30]]
-
 ########################################TESTING CONTENT#############################################
 
     X = np.array(basis_columns).T.tolist()
-
+    X = pd.DataFrame(X, columns=equities)
+    #first column of X is the first item in basis columns, second with second, ....
     #time for the math
     #first we will transform the raw data matrix X into deviation scores for matrix x
     #x = X - 11'X (1.0/n) where 
     # X is an n x k matrix of returns, x is an n x k matrix of deviation scores, and 1 is an n x 1 column 
     # vector of ones
-
-    n = len(X)
+    n = len(X.index)
     #one = 11'
     one = np.ones((n,n))
-
     #x = X - (1.0/n) * 11'X
     x = np.subtract(X, np.multiply( (1/n), np.matmul(one, X) ))
-
+    xt = x.transpose()
     #Then we will compute x'x, the k x k deviation sums of squares and cross products matrix for x
-    dev_ss_cp = np.matmul(x.transpose(), x)
-    
-    #then we will divide each term in the deviation sum of squares and cross products by n to create the variance 
-    #covariance matrix
+    #pandas dot is a matrix multiplication operation
+    dev_ss_cp = xt.dot(x)
     V  = np.multiply((1/n), dev_ss_cp)
-
-    #V should be a k x k (num assets x num assets) variance covariance matrix (aka covariance matrix) for the 
-    #columns input into the function
-    #The diagonal of V is the variance of returns for each equity and the covariance is represented by the 
-    #off diagonal elements of V
-
+    #V should be a k x k (num assets x num assets) variance covariance matrix
     return V
-
-# def getCovarianceMatrixOfReturns2(equities, start, end, basis="Adj Close"):
-#     basis_df = pdr.DataReader(["STZ"], 'yahoo', start=start, end=end)[basis]
-#     #V = np.multiply(basis_df.pct_change().dropna().cov(), (len(basis_df) - 1) / len(basis_df))
-#     #print(V)
-#     basis_col = basis_df["STZ"]
-#     change = basis_col.pct_change()
-#     variance = (change.std() * (len(change)-1)/(len(change)))**2
-#     print(variance)    
-#     #return V
 
 def getTickerStats(tickers, start, end):
         equity_df = []
@@ -91,22 +66,82 @@ def getRiskFreeRate(benchmark=["^IRX"], basis="Adj Close", start=date.today().st
     rf = getTickerStats(benchmark, start=start, end=end)
     return rf[0][basis][-1]
 
+def getMarketCapWeights(market_caps):
+    market_cap_weights = []
+    total_market_cap = sum(market_caps)
 
+    for cap in market_caps:
+        market_cap_weights.append(cap/total_market_cap)
+    
+    return market_cap_weights
 
+def calcImpliedEquilibriumReturns(tickers, start, end, basis="Adj Close"):
+    cov_matrix.values = getCovarianceMatrixOfReturns(tickers, start=start, end=end, basis=basis)
+    #risk_aversion_coef = getRiskAversionCoef()
+    risk_aversion_coef = 3
+    market_cap_weights = getMarketCapWeights([1.63, 227.72, 33.64, 904.62, 131.4])
+    #PI = l * E * W
+    return np.multiply(risk_aversion_coef, np.matmul(cov_matrix, market_cap_weights))
+
+    
 
 if __name__ == "__main__":
     # end = "2020-02-01"
     end = date.today().strftime("%Y-%m-%d")
     start = "2020-01-01"
-
     # equities = ["HEDJ", "VZ", "STZ", "GOOG", "AMGN", "EMR", "GILD", "FMC"]
     equities = ["HEDJ", "VZ", "STZ", "GOOG", "EMR"]
     #market_caps = [1.63, 227.72, 33.64, 904.62, 131.4, 39.39, 91.18, 11.97]
-
     market_caps = [1.63, 227.72, 33.64, 904.62, 131.4]
     var_cov_matrix = getCovarianceMatrixOfReturns(equities, start=start, end=end, basis="Adj Close")
     print("##############VARIANCE COVARIANCE MATRIX ###################\n")
     print(var_cov_matrix)
+    print()
+    print(var_cov_matrix.values)
+    print()
+    print(var_cov_matrix.values[0][0])
     print("\n###################################################\n")
-    rf = getRiskFreeRate()
-    print(rf)
+
+    # rf = getRiskFreeRate()
+    # print(rf)
+    # print(getMarketCapWeights([1.63, 227.72, 33.64, 904.62, 131.4]))
+    # print(sum(getMarketCapWeights([1.63, 227.72, 33.64, 904.62, 131.4])))
+    # print(calcImpliedEquilibriumReturns(equities, start=start, end=end, basis="Adj Close") * 100)
+
+
+
+
+
+
+
+
+# def getCovarianceMatrixOfReturns2(equities, start, end, basis="Adj Close"):
+#     basis_df = pdr.DataReader(["HEDJ"], 'yahoo', start=start, end=end)[basis]
+#     #V = np.multiply(basis_df.pct_change().dropna().cov(), (len(basis_df) - 1) / len(basis_df))
+#     #print(V)
+#     basis_col = basis_df["HEDJ"]
+#     change = basis_col.pct_change()
+#     variance = (change.std() * (len(change)-1)/(len(change)))**2
+#     print(variance)    
+#     #return V
+
+# def getCovarianceMatrixOfReturns3(equities, start, end, basis="Adj Close"):
+# #This implementation is based off of this example https://stattrek.com/matrix-algebra/covariance-matrix.aspx
+#     equity_df = getTickerStats(equities, start=start, end=end)
+#     basis_columns = getDailyReturns(equity_df)
+
+# ########################################TESTING CONTENT############################################
+# #each sub list represents a different "student's test scores" in math english and art
+#     # basis_columns = [[90,90,60,60,30], [60,90,60,60,30], [90,30,60,90,30]]
+# ########################################TESTING CONTENT#############################################
+
+#     X = np.array(basis_columns).T.tolist()
+#     n = len(X)
+#     #one = 11'
+#     one = np.ones((n,n))
+
+#     #x = X - (1.0/n) * 11'X
+#     x = np.subtract(X, np.multiply( (1/n), np.matmul(one, X) ))
+#     dev_ss_cp = np.matmul(x.transpose(), x)
+#     V  = np.multiply((1/n), dev_ss_cp)
+#     return pd.DataFrame(V, columns=equities, index=equities)
